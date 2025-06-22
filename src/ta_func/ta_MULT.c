9ba6d22e3b8ea81fad5433640491bf82b4a0771b
Chad Furman <chad@chadfurman.com>
2025-06-22 12:28:31 -0400

    ta_MULT updated with new loop var and re-gen'd

--- src/ta_func/ta_MULT.c
+++ src/ta_func/ta_MULT.c
@@ -159,19 +159,26 @@
 {
    /* insert local variable here */
    DECLARE_INDEX_VAR(outIdx)
-   DECLARE_INDEX_VAR(i)
+   DECLARE_LOOP_VAR(i)
 
 /**** START GENCODE SECTION 4 - DO NOT DELETE THIS LINE ****/
 /* Generated */ 
 /* Generated */ #ifndef TA_FUNC_NO_RANGE_CHECK
 /* Generated */ 
 /* Generated */    /* Validate the requested output range. */
+/* Generated */ #if defined( _RUST )
+/* Generated */    /* Skip negative checks for Rust since startIdx/endIdx are usize (unsigned) */
+/* Generated */    if( endIdx < startIdx ) {
+/* Generated */       return ENUM_VALUE(RetCode,TA_OUT_OF_RANGE_END_INDEX,OutOfRangeEndIndex);
+/* Generated */    }
+/* Generated */ #else
 /* Generated */    if( startIdx < 0 ) {
 /* Generated */       return ENUM_VALUE(RetCode,TA_OUT_OF_RANGE_START_INDEX,OutOfRangeStartIndex);
 /* Generated */    }
 /* Generated */    if( (endIdx < 0) || (endIdx < startIdx)) {
 /* Generated */       return ENUM_VALUE(RetCode,TA_OUT_OF_RANGE_END_INDEX,OutOfRangeEndIndex);
 /* Generated */    }
+/* Generated */ #endif
 /* Generated */ 
 /* Generated */ #if defined( _RUST )
 /* Generated */ 
@@ -254,14 +261,20 @@
 /* Generated */ #endif
 /* Generated */ {
 /* Generated */    DECLARE_INDEX_VAR(outIdx)
-/* Generated */    DECLARE_INDEX_VAR(i)
+/* Generated */    DECLARE_LOOP_VAR(i)
 /* Generated */  #ifndef TA_FUNC_NO_RANGE_CHECK
+/* Generated */  #if defined( _RUST )
+/* Generated */     if( endIdx < startIdx ) {
+/* Generated */        return ENUM_VALUE(RetCode,TA_OUT_OF_RANGE_END_INDEX,OutOfRangeEndIndex);
+/* Generated */     }
+/* Generated */  #else
 /* Generated */     if( startIdx < 0 ) {
 /* Generated */        return ENUM_VALUE(RetCode,TA_OUT_OF_RANGE_START_INDEX,OutOfRangeStartIndex);
 /* Generated */     }
 /* Generated */     if( (endIdx < 0) || (endIdx < startIdx)) {
 /* Generated */        return ENUM_VALUE(RetCode,TA_OUT_OF_RANGE_END_INDEX,OutOfRangeEndIndex);
 /* Generated */     }
+/* Generated */  #endif
 /* Generated */  #if defined( _RUST )
 /* Generated */  #else
 /* Generated */     #if !defined(_JAVA)
